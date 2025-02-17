490519521f454dc6da7b2ae082d7c9ed8f69ea9b
Norene Shelton <norenerubin@gmail.com>
2025-02-16 20:22:07 -0600

    fixing things

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ const renderToDom = (divId, toRender) => {
     renderToDom("#pinRepo", pinString)
   }
   
+
   /*const pinForm = () => {
     let pinFormString = `
   
@@ -90,6 +91,91 @@ const renderToDom = (divId, toRender) => {
   startApp()
   
   }
+
+
+
+  const repos = [
+    {
+      id: 1,
+      name: "Code-crafted-studio",
+      description: "A collection of meticulously crafted code, algorithms, and projects to explore and learn from."
+    },
+    {
+      id: 2,
+      name: "Jin-woo-tech-works",
+      description: "A tech workspace for Jin Woo's experiments, including web development, algorithms, and programming projects."
+    },
+    {
+      id: 3,
+      name: "Quantum-coder-chronicles",
+      description: "Dive into the realm of quantum computing, machine learning, and experimental code through Jin Woo's lens."
+    },
+    {
+      id: 4,
+      name: "AbyssalDev-Nexus",
+      description: "A hub for deep learning, blockchain, and high-performance computing."
+    },
+    {
+      id: 5,
+      name: "DimensionalCodeX",
+      description: "A repo experimenting with VR, AR, and metaverse development"
+    },
+    {
+      id: 6,
+      name: "Eclipse-Codeworks",
+      description: "A playground for creative coding, game development, and AI-driven applications."
+    }
+  ]
+
+  // DOM
+  const renderToDom = (divId, toRender) => {
+    const SelectedDiv = document.querySelector(divId);
+    selectedDiv.innnerHTML = toRender
+  };
+
+  // Function to build and display rpos
+  const reposDom = (repos) => {
+    let domString ="";
+  for (const repo of repos) {
+    domString += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h4 class="card-title">${repo.name}</h4>
+      <p class="card-text">${repo.description}</p>
+      </div>
+    </div>
+    `;
+  }
+  renderToDom("#repoApp", domString);
+};
+
+const form = document.querySelector("form");
+  
+
+  const createRepo = (e) => {
+    e.preventDefault();
+  
+ // Function to handle form submission and add a new repo
+      const newRepo = {
+        id: repos.length + 1,
+        name: document.querySelector("#name").value,
+        description: document.querySelector("#description").value,
+    };
+  
+    repos.push(newRepo);
+    reposDom(repos);
+    form.reset();
+  }
+  
+  form.addEventListener("submit", createRepo);
+  
+  const startApp = () => {
+    if (window.location.href.includes("repo.html")) {
+      reposDom(repos)
+    }
+  }
+  startApp();
+  
+  
 
 
 
